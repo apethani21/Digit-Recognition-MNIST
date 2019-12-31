b6061943f2c2610c5af8a20e1b07ede38ca36f03
Ajay Pethani <ajaypethani97@gmail.com>
2019-12-31 22:28:39 +0000

    refactored to use model of choice from models.py

--- fashion_cnn.py
+++ fashion_cnn.py
@@ -1,3 +1,4 @@
+import sys
 import pickle
 import logging
 import tensorflow as tf
@@ -18,31 +19,9 @@ def normalise(images, labels):
     return images, labels
 
 
-def get_model():
-    model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(32, (3,3), padding='same', 
-                           activation=tf.nn.relu, input_shape=(28, 28, 1)),
-    tf.keras.layers.MaxPooling2D((3, 3), strides=2),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Conv2D(32, (3,3), padding='same', 
-                           activation=tf.nn.relu),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.MaxPooling2D((3, 3), strides=2),
-    tf.keras.layers.Conv2D(32, (3,3), padding='same', 
-                           activation=tf.nn.relu),
-    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation=tf.nn.relu),
-    tf.keras.layers.Dense(10,  activation=tf.nn.softmax)
-    ])
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+def train(model_name, batch_size=32, epochs=15):
+    model = getattr(__import__('models'), model_name)()
     print(model.summary())
-    return model
-
-
-def train(batch_size=32, epochs=15):
     train_dataset, test_dataset, metadata = get_data()
     num_train_examples = metadata.splits['train'].num_examples
     train_dataset = (train_dataset
@@ -57,32 +36,34 @@ def train(batch_size=32, epochs=15):
     model_checkpoint = (tf
                         .keras
                         .callbacks
-                        .ModelCheckpoint('./config/fashion_cnn.h5', verbose=True)
-    )
-    global model
-    model = get_model()
+                        .ModelCheckpoint('./config/fashion_cnn.h5',
+                                         verbose=True))
     steps = int((num_train_examples/batch_size) + 1)
     history = model.fit(x=train_dataset,
                         epochs=epochs,
                         steps_per_epoch=steps,
-                        callbacks = [model_checkpoint])
+                        callbacks=[model_checkpoint])
     model.save('./config/fashion_cnn.h5')
     return history, steps, test_dataset
 
 
-def evaluate_model(steps, test_dataset):
+def evaluate_model(model, steps, test_dataset):
     acc = model.evaluate(test_dataset, steps=steps)
     print('Accuracy: ', acc)
 
-    
-def compute():
-    history, steps, test_dataset = train(batch_size=64, epochs=10)
+
+def compute(model, *args):
+    (history,
+     steps,
+     test_dataset) = train(model, batch_size=64, epochs=10, *args)
     with open('./config/history', 'wb') as f:
         pickle.dump(history.history, f)
-    evaluate_model(steps, test_dataset)
+    evaluate_model(model, steps, test_dataset)
 
 
 if __name__ == "__main__":
+    arg = sys.argv[1]
+    model_name = arg.split('=')[-1]
     logger = tf.get_logger()
     logger.setLevel(logging.ERROR)
-    compute()
+    compute(model_name)
