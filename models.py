2a5c03f4c9cdb1d6143773dfd755dd08604437b7
Ajay Pethani <ajaypethani97@gmail.com>
2020-01-29 23:41:16 +0000

    new model

--- models.py
+++ models.py
@@ -23,3 +23,25 @@ def model_one():
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     return model
+
+def model_two():
+    model = keras.Sequential([
+        layers.Conv2D(64, (3,3), padding='same',
+                      activation=tf.nn.relu, input_shape=(28, 28, 1)),
+        layers.MaxPooling2D((3, 3), strides=2),
+        layers.Dropout(0.2),
+        layers.Conv2D(64, (3,3), padding='same',
+                      activation=tf.nn.relu),
+        layers.Dropout(0.2),
+        layers.MaxPooling2D((3, 3), strides=2),
+        layers.Conv2D(64, (3,3), padding='same',
+                      activation=tf.nn.relu),
+        layers.MaxPooling2D((2, 2), strides=2),
+        layers.Flatten(),
+        layers.Dense(128, activation=tf.nn.relu),
+        layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
